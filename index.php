90836c05b85b9dc1aa52d6a04cfca8d6002a6e60
arunkumarsundar <92307320+arunkumarsundar@users.noreply.github.com>
2021-10-13 11:06:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World From Openshift! Newly added";
-?>
-
-</body>
-</html>
+  //process client request
+  header("Content-Type:application/json");
+  if(!empty($_GET['uname']) And !empty($_GET['upword']))
+  {
+    $varUname = $_GET['uname'];
+    $varUpword = $_GET['upword'];
+    //simple logic to validate the provided Username and UserPassword
+    if($varUname == "Arun" and $varUpword == "Maverick")
+    {
+      $response['Message'] = "Login successfull";
+      $response['Organisation'] = "Top Gun";
+      $response['Role'] = "Fighter pilot/coach";
+      $response['Track'] = "Take my breath away";
+      echo json_encode($response);
+    }
+    else 
+    { 
+      $response['Message'] = "Invalid user credentials";
+      $response['Useraction'] = "Next time try with valid credentials";
+      echo json_encode($response);
+    }
+  }
+  else
+  {
+    $response['Message'] = "Invalid JSON request";
+    $response['Useraction'] = "Contact developer in order to obtain correct JSON structure";
+    echo json_encode($response);
+  }
